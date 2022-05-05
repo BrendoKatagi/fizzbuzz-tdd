3724c5de6fdb99ee65c1b96661c021692242cf0b
Brendo Katagi <brendo.katagi@gmail.com>
2022-05-05 00:28:17 -0300

    test: return Fizz for multiples of 3

--- src/fizzbuz.spec.ts
+++ src/fizzbuz.spec.ts
@@ -14,4 +14,8 @@ describe("FizzBuzz", () => {
   test("Should return 2 for entry 2", () => {
     expect(fb.check(2)).toBe(2);
   })
+
+  test("Should return Fizz for multiples of 3", () => {
+    expect(fb.check(3)).toBe("Fizz");
+  })
 })
