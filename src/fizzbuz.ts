b4497004f342697ff13619b20a3ec8c9fc048275
Brendo Katagi <brendo.katagi@gmail.com>
2022-05-05 00:40:26 -0300

    feat: updated method to return fizz when the entry is a multiple of 5

--- src/fizzbuz.ts
+++ src/fizzbuz.ts
@@ -2,9 +2,9 @@ export class Fizzbuzz {
 
   check (num: number): string {
 
-    if(num % 3 == 0){
-      return "Fizz";
-    }
+    if(num % 3 == 0) return "Fizz";
+
+    if(num % 5 == 0) return "Buzz";
 
     return num.toString();
   }
