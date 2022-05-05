e480dbad14ba7b611a2faa5e4e56c1284be07d7b
Brendo Katagi <brendo.katagi@gmail.com>
2022-05-05 00:46:17 -0300

    feat: updated method to return fizzbuzz when the entry is a multiple of both 3 and 5

--- src/fizzbuz.ts
+++ src/fizzbuz.ts
@@ -2,6 +2,8 @@ export class Fizzbuzz {
 
   check (num: number): string {
 
+    if(num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
+
     if(num % 3 == 0) return "Fizz";
 
     if(num % 5 == 0) return "Buzz";
