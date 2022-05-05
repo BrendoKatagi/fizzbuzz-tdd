8cca26ddec04119043ccc7f2677405df46128f7e
Brendo Katagi <brendo.katagi@gmail.com>
2022-05-05 00:29:57 -0300

    test: updated test cases to accept strings

--- src/fizzbuz.spec.ts
+++ src/fizzbuz.spec.ts
@@ -8,11 +8,11 @@ beforeEach(() => {
 
 describe("FizzBuzz", () => {
   test("Should return 1 for entry 1", () => {
-    expect(fb.check(1)).toBe(1);
+    expect(fb.check(1)).toBe("1");
   })
 
   test("Should return 2 for entry 2", () => {
-    expect(fb.check(2)).toBe(2);
+    expect(fb.check(2)).toBe("2");
   })
 
   test("Should return Fizz for multiples of 3", () => {
